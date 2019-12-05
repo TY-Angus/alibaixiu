fbb803075df4d12124a13abefb1585fc91bfef2f
angus <2936454740@qq.com>
2019-12-05 10:30:05 +0800

    需求7添加用户修改功能

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -22,8 +22,8 @@ $('#userForm').on('submit', function () {
 $.ajax({
 	type: 'get',
 	url: '/users',
-	success: function (data) {
-		let html = template('userTpl',{data})
+	success: function (response) {
+		let html = template('userTpl', { data: response })
 		$('#userBox').html(html)
 	},
 	error: function () {
@@ -31,25 +31,62 @@ $.ajax({
 	}
 })
 
-$('#avatar').on('change',function(){
+// 需求6 图片与解析和图片上传
+$('#modifyBox').on('change', '#avatar', function () {
 	let formData = new FormData()
 	// this.files[0]指第一张图片
-	formData.append('avatar',this.files[0])
+	formData.append('avatar', this.files[0])
 	$.ajax({
-		type:'post',
-		url:'/upload',
-		data:formData,
+		type: 'post',
+		url: '/upload',
+		data: formData,
 		// 告诉$.ajax()方法不要解析请求参数
-		processData:false,
+		processData: false,
 		// 告诉$.ajax()方法不要设置参数的类型
-		contentType:false,
-		success:function(response){
+		contentType: false,
+		success: function (response) {
 			// response[0].avatar返回的是图片的路径
 			$('#preview').attr('src', response[0].avatar)
 			$('#hiddenAvatar').val(response[0].avatar)
 		}
-		
+
 	})
 
 })
+
+// 需求7 添加用户编辑功能
+$('#userBox').on('click', '.edit', function () {
+	let id = $(this).attr('data-id')
+	$.ajax({
+		type: 'GET',
+		url: '/users/' + id,
+		success: function (response) {
+			let html = template('modifyTpl', response)
+			$('#modifyBox').html(html)
+		}
+	})
+})
+$('#modifyBox').on('submit', '#modifyForm', function () {
+	let formData = $(this).serialize()
+	let id = $(this).attr('data-id')
+	$.ajax({
+		type: 'put',
+		url: '/users/' + id,
+		data: formData,
+		success: function () {
+			location.reload()
+		},
+		error: function () {
+			alert('注册用户失败')
+		}
+	})
+	return false
+})
+
+
+
+
+
+
+
 
