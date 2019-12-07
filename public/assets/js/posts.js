26b77bb3533c35028fd135e08bccaea6565d595f
angus <2936454740@qq.com>
2019-12-07 10:21:52 +0800

    需求14筛选功能

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -1,15 +1,34 @@
 // 需求13文章列表渲染
-$.ajax({
-    type: 'get',
-    url: '/posts',
-    success: function (response) {
-        let html = template('postListTpl', response)
-        $('#postListBox').html(html)
-    }
-})
+function render(data, page) {
+    data.page = page || 1
+    $.ajax({
+        type: 'get',
+        url: '/posts',
+        data: data,
+        success: function (response) {
+            let html = template('postListTpl', response)
+            $('#postListBox').html(html)
+        }
+    })
+}
 // 处理日期格式
 function formateDate(date) {
     let dateAt = new Date(date)
     return dateAt.getFullYear() + '-' + (dateAt.getMonth() + 1) + '-' + dateAt.getDate()
 }
+render({}, 1)
 
+// 需求14筛选功能
+$.ajax({
+    type: 'get',
+    url: '/categories',
+    success: function (response) {
+        let html = template('categoryTpl', { data: response })
+        $('#categoryBox').html(html)
+    }
+})
+$('#filterForm').on('submit', function () {
+    let formData = $(this).serialize()
+    render(formData, 1)
+    return false
+})
