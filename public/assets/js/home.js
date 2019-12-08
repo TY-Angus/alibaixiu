67ba3905c887c5aa83955e11038492de78542488
angus <2936454740@qq.com>
2019-12-08 20:52:56 +0800

    修复了一些代码书写不规范

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -3,8 +3,24 @@ $.ajax({
 	type: 'get',
 	url: '/slides',
 	success: function (response) {
-		var html = template('swipeTpl', {data: response});
+		let html = template('swipeTpl', {data: response});
 		$('#swipeBox').html(html)
+		let swiper = Swipe(document.querySelector('.swipe'), {
+			auto: 3000,
+			transitionEnd: function (index) {
+			  // index++;
+			  $('.cursor span').eq(index).addClass('active').siblings('.active').removeClass('active');
+			}
+		  });
+		  // 上/下一张
+		  $('.swipe .arrow').on('click', function () {
+			let _this = $(this);
+			if(_this.is('.prev')) {
+			  swiper.prev();
+			} else if(_this.is('.next')) {
+			  swiper.next();
+			}
+		  })
 	}
 });
 // 需求27 最新发布展示
@@ -12,7 +28,7 @@ $.ajax({
     type:'get',
     url:'/posts/lasted',
     success:function(response){
-		console.log(response)
+	
         let html = template('lastTpl',{data:response})
         $('#lastBox').html(html)
     }
